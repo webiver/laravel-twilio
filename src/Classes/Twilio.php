2e2c13ff65cd2169ecf3d02277612c7a7966b225
EngMohammed40 <engmohammedahmed40@gmail.com>
2023-05-05 00:04:13 +0300

    Add SMS & Whatsapp Functions

--- src/Classes/Twilio.php
+++ src/Classes/Twilio.php
@@ -7,10 +7,16 @@ class Twilio {
 
     private $sid;
     private $token;
+    private $smsPhone;
+    private $whatsappPhone;
+    private $twilio;
     public function __construct()
     {
         $this->sid = env('TWILIO_ACCOUNT_SID');
         $this->token = env('TWILIO_AUTH_TOKEN');
+        $this->smsPhone = env('TWILIO_SMS_PHONE_NUMBER');
+        $this->whatsappPhone = env('TWILIO_WHATSAPP_PHONE_NUMBER');
+        $this->twilio = new Client($this->sid, $this->token);
     }
 
     public function getSid(){
@@ -21,8 +27,26 @@ class Twilio {
         return $this->token;
     }
 
-    public function sms(){
-        $twilio = new Client($this->sid, $this->token);
-        return $twilio;
+    public function sms($toPhone,$message){
+        $message = $this->twilio->messages
+        ->create($toPhone,
+            [
+                "from" => $this->smsPhone,
+                "body" => $message
+            ]
+        );
+        return $message;
+    }
+
+
+    public function whatsapp($toPhone,$message){
+        $message = $this->twilio->messages
+        ->create("whatsapp:".$toPhone,
+            [
+                "from" => "whatsapp:".$this->whatsappPhone,
+                "body" => $message
+            ]
+        );
+        return $message;
     }
 }
